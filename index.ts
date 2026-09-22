6e6a32abfdb876cba42c6b99c1ac308df4f7560b
Weantim <nosoker1134@gmail.com>
2026-09-22 16:17:42 +0300

    menu

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-let update = 1;
+let c = Number(prompt("Меню выберите пункт"));
+console.log("1. Ввод двух чисел");
+console.log("2. Сложение");
+console.log("3. Вычитание");
+console.log("4. Деление");
+console.log("5. Степень");
+
+switch () {
+    case 1:console.log();
+    case 2:console.log();
+    case 3:console.log();
+    case 4:console.log();
+    case 5:console.log();
+    case 6:console.log();
+        default:
+}
